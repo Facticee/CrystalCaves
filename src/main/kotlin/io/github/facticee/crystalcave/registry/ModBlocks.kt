ef4a92263b8ddbec0189c69f0301413b48debb23
Facticee <timon.eliah.plum@gmail.com>
2026-07-19 21:45:31 +0200

    more blocks

--- src/main/kotlin/io/github/facticee/crystalcave/registry/ModBlocks.kt
+++ src/main/kotlin/io/github/facticee/crystalcave/registry/ModBlocks.kt
@@ -13,9 +13,20 @@ object ModBlocks {
 
     lateinit var CRYSTAL_STONE: Block
 
-
     lateinit var SAPPHIRE_ORE: Block
 
+    lateinit var MOONSTONE_ORE: Block
+
+    lateinit var TOPAZ_ORE: Block
+
+    lateinit var SAPPHIRE_BLOCK: Block
+    lateinit var MOONSTONE_BLOCK: Block
+
+    lateinit var TOPAZ_BLOCK: Block
+    lateinit var TOPAZ_SLAB: Block
+    lateinit var TOPAZ_STAIRS: Block
+    lateinit var TOPAZ_WALL: Block
+    lateinit var TOPAZ_PILLAR: Block
 
     // "basic" block
     private fun register(name: String, properties: BlockBehaviour.Properties): Block {
@@ -42,10 +53,54 @@ object ModBlocks {
                 .sound(SoundType.STONE)
         )
 
-
         SAPPHIRE_ORE = registerSpecial("sapphire_ore", BlockBehaviour.Properties.of().strength(3f)) {
             CrystalOreBlock(it)
         }
 
+
+        MOONSTONE_ORE = registerSpecial("moonstone_ore", BlockBehaviour.Properties.of().strength(3f)) {
+            CrystalOreBlock(it)
+        }
+
+
+        TOPAZ_ORE = registerSpecial("topaz_ore", BlockBehaviour.Properties.of().strength(3f)) {
+            CrystalOreBlock(it)
+        }
+
+
+        SAPPHIRE_BLOCK = register(
+            "sapphire_block",
+            BlockBehaviour.Properties.of()
+                .strength(5f)
+                .sound(SoundType.AMETHYST)
+        )
+
+        MOONSTONE_BLOCK = register(
+            "moonstone_block",
+            BlockBehaviour.Properties.of()
+                .strength(5f)
+                .sound(SoundType.AMETHYST)
+        )
+
+        TOPAZ_BLOCK = register(
+            "topaz_block",
+            BlockBehaviour.Properties.of().strength(4f)
+        )
+
+        TOPAZ_SLAB = registerSpecial("topaz_slab", BlockBehaviour.Properties.of().strength(4f)) {
+            SlabBlock(it)
+        }
+
+        TOPAZ_STAIRS = registerSpecial("topaz_stairs", BlockBehaviour.Properties.of().strength(4f)) {
+            StairBlock(TOPAZ_BLOCK.defaultBlockState(), it)
+        }
+
+        TOPAZ_WALL = registerSpecial("topaz_wall", BlockBehaviour.Properties.of().strength(4f)) {
+            WallBlock(it)
+        }
+
+        TOPAZ_PILLAR = registerSpecial("topaz_pillar", BlockBehaviour.Properties.of().strength(4f)) {
+            RotatedPillarBlock(it)
+        }
     }
 }
