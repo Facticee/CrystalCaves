60c8a0c6e2a8fff5e87daa6b61d19f9ca0b75d92
Facticee <timon.eliah.plum@gmail.com>
2026-07-19 22:54:31 +0200

    creative tab test

--- src/main/kotlin/io/github/facticee/crystalcave/registry/ModCreativeTab.kt
+++ src/main/kotlin/io/github/facticee/crystalcave/registry/ModCreativeTab.kt
@@ -1,10 +1,14 @@
 package io.github.facticee.crystalcave.registry
 
 import io.github.facticee.crystalcave.CrystalCaveMod
+import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab
+import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.Identifier
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.CreativeModeTab
+import net.minecraft.world.item.ItemStack
 
 object ModCreativeTab {
 
@@ -17,6 +21,22 @@ object ModCreativeTab {
             )
         )
 
+    val CRYSTAL_CAVE_TAB: CreativeModeTab = Registry.register(
+        BuiltInRegistries.CREATIVE_MODE_TAB,
+        CRYSTAL_CAVE_KEY,
+        FabricCreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.crystalcave.crystal_cave"))
+            .icon {
+                ItemStack(ModItems.SAPPHIRE)
+            }
+            .displayItems { _, output ->
+
+                // TEST
+                output.accept(ModItems.SAPPHIRE)
+                
+            }
+            .build()
+    )
 
     fun registerAll() {
 
