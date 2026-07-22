e8269c1eb64e7858fdf023d43e7068ce507ce1c8
Facticee <timon.eliah.plum@gmail.com>
2026-07-22 17:29:40 +0200

    CrystalFissureConfig.kt added more ways to configure the cave

--- src/main/kotlin/io/github/facticee/crystalcave/world/feature/CrystalFissureConfig.kt
+++ src/main/kotlin/io/github/facticee/crystalcave/world/feature/CrystalFissureConfig.kt
@@ -7,7 +7,13 @@ import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfigur
 data class CrystalFissureConfig(
     val palette: CrystalPalette,
     val length: IntRange2,
-    val crossRadius: IntRange2
+    val crossRadius: IntRange2,
+    val tilt: Float,
+    val innerThickness: Float,
+    val middleThickness: Float,
+    val outerThickness: Float,
+    val shardChance: Float,
+    val vertical: Boolean
 ) : FeatureConfiguration {
 
     companion object {
@@ -15,7 +21,13 @@ data class CrystalFissureConfig(
             instance.group(
                 CrystalPalette.CODEC.fieldOf("palette").forGetter(CrystalFissureConfig::palette),
                 IntRange2.CODEC.fieldOf("length").forGetter(CrystalFissureConfig::length),
-                IntRange2.CODEC.fieldOf("cross_radius").forGetter(CrystalFissureConfig::crossRadius)
+                IntRange2.CODEC.fieldOf("cross_radius").forGetter(CrystalFissureConfig::crossRadius),
+                Codec.FLOAT.fieldOf("tilt").forGetter(CrystalFissureConfig::tilt),
+                Codec.FLOAT.fieldOf("inner_thickness").forGetter(CrystalFissureConfig::innerThickness),
+                Codec.FLOAT.fieldOf("middle_thickness").forGetter(CrystalFissureConfig::middleThickness),
+                Codec.FLOAT.fieldOf("outer_thickness").forGetter(CrystalFissureConfig::outerThickness),
+                Codec.FLOAT.fieldOf("shard_chance").forGetter(CrystalFissureConfig::shardChance),
+                Codec.BOOL.fieldOf("vertical").forGetter(CrystalFissureConfig::vertical)
             ).apply(instance, ::CrystalFissureConfig)
         }
     }
